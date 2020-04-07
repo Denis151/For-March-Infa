f4224471eb20533cb5d31b69f62f1fd786ab98c1
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:42:36 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def func(x, y=7):
+ def func(x, y=7):
   return  x * y   
 print(func(3, 7))
 
