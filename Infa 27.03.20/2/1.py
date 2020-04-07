412a5bf96c2ff14a0859679d84bb8576d0b9be78
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:54:04 +0300

    Update 1.py

--- Infa 27.03.20/2/1.py
+++ Infa 27.03.20/2/1.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class KZI_21:
+class KZI_21:
   def __init__(self, a1="Київ",a2="- столиця України"):
     self.p1 = a1
     self.p2 = a2
