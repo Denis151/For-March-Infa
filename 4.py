2a4961c643229475e9782829666fc21148a85dde
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:41:38 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,12 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def func(x, y=7):
+  return  x * y   
+print(func(3, 7))
+
+m = [4,6,7,9,10]
+x = int(input())
+y = int(input())
+def func_a(a,b):
+ return (a * b) 
+print(func_a(m[x], m[y]))
