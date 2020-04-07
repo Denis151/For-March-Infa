3dc7e1733e859ecbabbfc2444215b034a3e6cdf0
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:45:55 +0300

    Update 3.py

--- Infa 01.03.20/3.py
+++ Infa 01.03.20/3.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a1 = 3/7+5*8/3
+a1 = 3/7+5*8/3
 a2 = lambda x,y: 2*x+x/y
 a3 = lambda x,y: (3*x+5)/(y-3)
 print(a1)
