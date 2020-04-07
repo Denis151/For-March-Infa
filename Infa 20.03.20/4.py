88182a88fdfa7aadba5929365a99e06ce2058f61
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:50:12 +0300

    Update 4.py

--- Infa 20.03.20/4.py
+++ Infa 20.03.20/4.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = [3,5,2,7]
+a = [3,5,2,7]
 b = (4,1,5,6)
 
 def math_a(a,b,c,d):
