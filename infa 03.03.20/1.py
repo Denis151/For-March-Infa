d4d28d924bb82ebaa6bf3bf23da3b9a6c3daa901
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:47:15 +0300

    Update 1.py

--- infa 03.03.20/1.py
+++ infa 03.03.20/1.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> r = [4, 5.3, 6.6, 7.9, 9.2]
+r = [4, 5.3, 6.6, 7.9, 9.2]
 h = [4, 5.5, 7, 8.5, 10]
 
 
