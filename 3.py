b927d8bff20a9355e97b24b4503df532c482b7f2
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:42:26 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a1 = 3/7+5*8/3
-a2 = lambda x,y: 2*x+x/y
-a3 = lambda x,y: (3*x+5)/(y-3)
-print(a1)
-print(a2(5, 7))
-print(a3(3, 4))
