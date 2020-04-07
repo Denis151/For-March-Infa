c6aa43d36e35ab56eb845b88630e4817cb65a482
Denis151 <56153920+Denis151@users.noreply.github.com>
2020-04-07 12:45:37 +0300

    Update 2.py

--- Infa 01.03.20/2.py
+++ Infa 01.03.20/2.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def func(*s):
+def func(*s):
  return sum(s[1] + s[1])
  
 a = 1,3,5,7,9
